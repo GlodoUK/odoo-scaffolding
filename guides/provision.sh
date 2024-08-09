dd7acc4466a3c001b48875738658e923ee8548aa
Elliott Bristow <elliott@glo.systems>
2024-08-09 11:10:37 +0200

    Add inquirer library to provisioning

--- guides/provision.sh
+++ guides/provision.sh
@@ -168,6 +168,7 @@ install_pipx() {
   ~/.local/bin/pipx install copier
   ~/.local/bin/pipx install invoke
   ~/.local/bin/pipx install pre-commit
+  ~/.local/bin/pipx install inquirer
   
   grep -qxF 'export PATH=$PATH:~/.local/bin/' ~/.bashrc || echo 'export PATH=$PATH:~/.local/bin/' >> ~/.bashrc
 
